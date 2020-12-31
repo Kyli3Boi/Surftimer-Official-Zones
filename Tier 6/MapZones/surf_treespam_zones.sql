c6e9fc8600616cbf3488aacc1d6f709bb6b16176
Kyli3Boi <64275139+Kyli3Boi@users.noreply.github.com>
2020-12-31 13:20:41 +0200

    Add map

---
+++ Tier 6/MapZones/surf_treespam_zones.sql
@@ -0,0 +1,7 @@
+INSERT INTO `ck_zones` (`mapname`, `zoneid`, `zonetype`, `zonetypeid`, `pointa_x`, `pointa_y`, `pointa_z`, `pointb_x`, `pointb_y`, `pointb_z`, `vis`, `team`, `zonegroup`, `zonename`, `hookname`, `targetname`, `onejumplimit`, `prespeed`) VALUES
+('surf_treespam', 0, 1, 0, 254.915, 737.325, 456.031, -254.758, 990.933, 256.031, 0, 0, 0, 'Start 0', 'None', 'player', 1, 350),
+('surf_treespam', 1, 3, 0, 3585.1, 254.896, -2465.97, 4606.91, -254.916, -2815.97, 0, 0, 0, 'Start 0', 'None', 'player', 1, 350),
+('surf_treespam', 2, 3, 1, -3039.2, 14623, 223.031, -1056.73, 14135.6, 0.03125, 0, 0, 0, 'Start 0', 'None', 'player', 1, 350),
+('surf_treespam', 3, 3, 2, -5153.22, -10494.7, 4122.03, -7135.19, -10273.1, 3840.03, 0, 0, 0, 'Start 0', 'None', 'player', 1, 350),
+('surf_treespam', 4, 3, 3, -6336, -512, -993, -5920, -256, -799, 0, 0, 0, 'Start 0', 'zone_stage5', 'player', 1, 350),
+('surf_treespam', 5, 2, 0, -3039.42, 12033.2, 2270.03, -1057.23, 13279, 1568.03, 0, 0, 0, 'Start 0', 'None', 'player', 1, 350);
