5cf3e77c6fda05f27f90e3bc6081cc87fbb96727
Kyli3Boi <64275139+Kyli3Boi@users.noreply.github.com>
2021-01-10 11:15:11 +0200

    Add maps

---
+++ Tier 1/MapZones/surf_tendies_zones.sql
@@ -0,0 +1,13 @@
+INSERT INTO `ck_zones` (`mapname`, `zoneid`, `zonetype`, `zonetypeid`, `pointa_x`, `pointa_y`, `pointa_z`, `pointb_x`, `pointb_y`, `pointb_z`, `vis`, `team`, `zonegroup`, `zonename`, `hookname`, `targetname`, `onejumplimit`, `prespeed`) VALUES
+('surf_tendies', 0, 1, 0, -1793, 10495, 13311, -1023, 12033, 13505, 0, 0, 0, 'Start 0', 'map_startzone', 'player', 1, 350),
+('surf_tendies', 1, 4, 0, 10703, -641, 9023, 11809, -511, 11481, 0, 0, 0, 'Start 0', 'map_cp1', 'player', 1, 350),
+('surf_tendies', 2, 4, 1, -14593, -2561, 7295, -7679, -2431, 12289, 0, 0, 0, 'Start 0', 'map_cp2', 'player', 1, 350),
+('surf_tendies', 3, 4, 2, -9729, -5697, -2561, -6143, -5567, -383, 0, 0, 0, 'Start 0', 'map_cp3', 'player', 1, 350),
+('surf_tendies', 4, 4, 3, 9599, 6143, -3969, 13185, 6273, -2303, 0, 0, 0, 'Start 0', 'map_cp4', 'player', 1, 350),
+('surf_tendies', 5, 2, 0, -15361, 7167, -5633, -12672, 15873, -1535, 0, 0, 0, 'Start 0', 'map_endzone', 'player', 1, 350),
+('surf_tendies', 6, 1, 1, 12543, -4289, 6079, 13569, -3199, 6337, 0, 0, 1, 'Bonus 1', 'b1_xbmann_startzone', 'player', 1, 350),
+('surf_tendies', 7, 2, 1, -12225, -12417, 3903, -10815, -11520, 5569, 0, 0, 1, 'Bonus 1', 'b1_xbmann_endzone', 'player', 1, 350),
+('surf_tendies', 8, 1, 1, -513, -5889, 3903, 257, -5439, 4097, 0, 0, 2, 'Bonus 2', 'b2_dream_startzone', 'player', 1, 350),
+('surf_tendies', 9, 2, 1, -1537, -5793, 2831, -1023, -5536, 3137, 0, 0, 2, 'Bonus 2', 'b2_dream_endzone', 'player', 1, 350),
+('surf_tendies', 10, 1, 1, 10239, 6143, -1601, 12545, 7169, -767, 0, 0, 3, 'Bonus 3', 'b3_granis_startzone', 'player', 1, 350),
+('surf_tendies', 11, 2, 1, 10369, -1535, 9023, 12159, -639, 9537, 0, 0, 3, 'Bonus 3', 'b3_granis_endzone', 'player', 1, 350);
---
+++ Tier 2/MapZones/surf_crackdesert_evo2_zones.sql
@@ -0,0 +1,12 @@
+INSERT INTO `ck_zones` (`mapname`, `zoneid`, `zonetype`, `zonetypeid`, `pointa_x`, `pointa_y`, `pointa_z`, `pointb_x`, `pointb_y`, `pointb_z`, `vis`, `team`, `zonegroup`, `zonename`, `hookname`, `targetname`, `onejumplimit`, `prespeed`) VALUES
+('surf_crackdesert_evo2', 0, 1, 0, -448.859, -200.939, 988.031, -1022.87, -510.752, 768.031, 0, 0, 0, 'Start 0', 'None', 'player', 1, 350),
+('surf_crackdesert_evo2', 1, 3, 0, -431.114, -511.076, 1008.03, -1.06983, 127.221, 769.031, 0, 0, 0, 'Start 0', 'None', 'player', 1, 350),
+('surf_crackdesert_evo2', 2, 3, 1, -448.991, -183.088, 988.031, -1022.88, 126.909, 769.031, 0, 0, 0, 'Start 0', 'None', 'player', 1, 350),
+('surf_crackdesert_evo2', 3, 2, 0, -3241.17, 4137.24, -709.969, -4086.93, 4782.76, -999.969, 0, 0, 0, 'Start 0', 'None', 'player', 1, 350),
+('surf_crackdesert_evo2', 4, 1, 0, -6426.91, 7333.02, -265.969, -6934.2, 7081.12, -455.969, 0, 0, 1, 'Bonus 1', 'None', 'player', 1, 350),
+('surf_crackdesert_evo2', 5, 2, 0, -6423.24, 7096.96, -265.969, -6160.66, 7334.99, -455.969, 0, 0, 1, 'Bonus 1', 'None', 'player', 1, 350),
+('surf_crackdesert_evo2', 6, 2, 1, -6937.02, 7096.83, -265.969, -7199.53, 7334.83, -455.969, 0, 0, 1, 'Bonus 1', 'None', 'player', 1, 350),
+('surf_crackdesert_evo2', 7, 1, 0, -9984.97, -6016.53, 440.031, -10367.9, -7167.71, 200.031, 0, 0, 2, 'Bonus 2', 'None', 'player', 1, 350),
+('surf_crackdesert_evo2', 8, 2, 0, 3521.33, -6264.85, 215.031, 3934.78, -6919.28, -183.969, 0, 0, 2, 'Bonus 2', 'None', 'player', 1, 350),
+('surf_crackdesert_evo2', 9, 1, 1, -3273, 4311, -2473, -3055, 4449, -2367, 0, 0, 3, 'bonus 3', 'bonus3start', 'player', 1, 350),
+('surf_crackdesert_evo2', 10, 2, 1, -3055, 4311, -2241, -2879, 5193, -2191, 0, 0, 3, 'bonus 3', 'b3End', 'player', 1, 350);
